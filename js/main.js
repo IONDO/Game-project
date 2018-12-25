dda23ab96a74324ab5dd457656d76c7d9d1b7423
iondo <ines.ondo.baka@gmail.com>
2018-12-25 14:11:56 +0100

    add pause + resume action and ball to bounce around the playing area

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ const context = canvas.getContext("2d");
 context.fillStyle = "#0081D7";
 context.fillRect(0, 0, 580, 560);
 const updatesPerSecond = 35;
-let row = canvas.width / 4
-let column = canvas.height - 10;
-let addRow = 4;
-let addColumn = -4;
+const playingArea = {
+    width: 580,
+    height: 560
+}
+let x = canvas.width / 4
+let y = canvas.height - 10;
+let speedX = 4;
+let speedY = -4;
 
 function drawBall() {
     context.beginPath();
     context.fillStyle = "white";
-    context.arc(row, column, 15, 0, Math.PI * 2);
+    context.arc(x, y, 15, 0, Math.PI * 2);
     context.fill();
     context.closePath();
 }
@@ -21,21 +25,33 @@ function draw() {
     context.fillRect(0, 0, 580, 560);
     drawBall();
     context.fillStyle = "white";
-    context.fillRect(250, 500, 100, 20);
+    context.fillRect(250, 500, 90, 20);
     requestAnimationFrame(draw);
 }
+
 function update() {
-    row += addRow;
-    column += addColumn;
+    if (x <= 0 || x >= playingArea.width) {
+        console.log("plonch!")
+        speedX = -speedX;
+    }
+    if (y <= 0 || y >= playingArea.height) {
+        console.log("plonch!")
+        speedY = -speedY;
+    }
+    x += speedX;
+    y += speedY;
 }
-setInterval(update, 1000/updatesPerSecond);
-requestAnimationFrame(draw);
+
 context.fillStyle = "white";
 context.fillRect(250, 500, 100, 20);
-const button = document.getElementById("start-btn");
-button.onclick = function () {
-    document.getElementById("initial-screen").style = "display: none",
-        document.getElementById("game-screen").style = "display: "
+const startButton = document.getElementById("start-btn");
+let interval = undefined;
+startButton.onclick = function () {
+    document.getElementById("initial-screen").style = "display: none";
+    document.getElementById("game-screen").style = "display: ";
+    interval = setInterval(update, 1000 / updatesPerSecond);
+    requestAnimationFrame(draw);
+
     /* const game = document.getElementById("game-screen");
     let timeout = window.setTimeout(() => {
         game.style = "display: none";
@@ -46,4 +62,16 @@ button.onclick = function () {
         winnerScreen.style = "display: none";
         document.getElementById("loser-screen").style = "display: "
     }, 6000); */
+}
+
+const pauseButton = document.getElementById("pause");
+pauseButton.onclick = function () {
+    if (interval) {
+        clearInterval(interval);
+        interval = undefined;
+        pauseButton.innerText = "Resume"
+    } else {
+        interval = setInterval(update, 1000 / updatesPerSecond);
+        pauseButton.innerText = "Pause"
+    }
 }
