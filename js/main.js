5ae4c6c45d3543e8f2c4d0b0af4cf3679883618e
iondo <ines.ondo.baka@gmail.com>
2018-12-25 10:50:02 +0100

    add pause button to the side and make ball to move

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 context.fillStyle = "#0081D7";
-context.fillRect(0, 0, 680, 560);
-context.beginPath();
+context.fillRect(0, 0, 580, 560);
+const updatesPerSecond = 35;
+let row = canvas.width / 4
+let column = canvas.height - 10;
+let addRow = 4;
+let addColumn = -4;
+
+function drawBall() {
+    context.beginPath();
+    context.fillStyle = "white";
+    context.arc(row, column, 15, 0, Math.PI * 2);
+    context.fill();
+    context.closePath();
+}
+
+function draw() {
+    context.fillStyle = "#0081D7";
+    context.fillRect(0, 0, 580, 560);
+    drawBall();
+    context.fillStyle = "white";
+    context.fillRect(250, 500, 100, 20);
+    requestAnimationFrame(draw);
+}
+function update() {
+    row += addRow;
+    column += addColumn;
+}
+setInterval(update, 1000/updatesPerSecond);
+requestAnimationFrame(draw);
 context.fillStyle = "white";
-context.arc(200, 100, 15, 0, Math.PI * 2);
-context.fill();
-context.closePath();
-context.fillStyle = "white";
-context.fillRect(100, 300, 100, 20);
+context.fillRect(250, 500, 100, 20);
 const button = document.getElementById("start-btn");
 button.onclick = function () {
     document.getElementById("initial-screen").style = "display: none",
